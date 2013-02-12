9d6b49b1031f2b179fe5d080c6f6142103054db4
jose miguel <miguel.gamazo@galiclick.com>
2013-02-12 11:12:39 +0100

    Mas mejoras

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,5 +1,3 @@
 Spree::UserSessionsController.class_eval do
-  include Spree::Core::ControllerHelpers
-  include Spree::Core::StoreHelpers
-  layout '/tienda/sessions'
+  layout 'tienda/sessions'
 end
