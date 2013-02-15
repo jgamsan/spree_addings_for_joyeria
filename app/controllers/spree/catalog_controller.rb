b426d77b5a68aee29e46b9a5886bde60279c9311
jose miguel <miguel.gamazo@galiclick.com>
2013-02-15 20:26:51 +0100

    Mas mejoras

--- app/controllers/spree/catalog_controller.rb
+++ app/controllers/spree/catalog_controller.rb
@@ -2,6 +2,12 @@ module Spree
   class CatalogController < BaseController
     layout '/tienda/index_tienda'
     def index
+      @taxonomies = Spree::Taxonomy.includes(:root => :children).first
+      @t_root = @taxonomies.root
+      @taxons = @t_root.children
+      @oro = @taxons.first
+      @plata = @taxons.second
+      @nueva = @taxons.last
     end
   end
 end
