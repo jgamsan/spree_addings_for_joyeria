c9c6fec2c88b7e8002c83f90962f91469e3dc129
jose miguel <miguel.gamazo@galiclick.com>
2013-02-16 10:38:09 +0100

    Mas mejoras

--- app/controllers/spree/title_page_controller.rb
+++ app/controllers/spree/title_page_controller.rb
@@ -2,6 +2,12 @@ module Spree
   class TitlePageController < BaseController
     layout "/tienda/portada"
     def index
+      @taxonomies = Spree::Taxonomy.includes(:root => :children).first
+      @t_root = @taxonomies.root
+      @taxons = @t_root.children
+      @oro = @taxons.first
+      @plata = @taxons.second
+      @nueva = @taxons.last
     end
   end
 end
